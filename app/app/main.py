c5c4878efc5aac0f2bc37e01f0c084a63eb9e5c5
Karol Szpakowski <karol.szpakowski@gmail.com>
2023-12-21 19:59:23 +0100

    Refactor item retrieval to separate /api route
    Introduced a new FastAPI instance to handle API endpoints under the
    '/api' prefix, improving the modularity of our codebase.

--- app/app/main.py
+++ app/app/main.py
@@ -5,8 +5,11 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+api = FastAPI(openapi_prefix="/api")
+app.mount("/api", api)
+
 app.mount("/", StaticFiles(directory="static", html = True), name="static")
 
-@app.get("/items/{item_id}")
+@api.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
