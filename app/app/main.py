4239c3c85b52294b0a79e8fd0e5fb294a546c21d
Karol Szpakowski <karol.szpakowski@gmail.com>
2023-12-22 09:13:36 +0100

    Remove frontend app and associated configurations

--- app/app/main.py
+++ app/app/main.py
@@ -1,14 +1,11 @@
 from typing import Union
 
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 api = FastAPI(openapi_prefix="/api")
 app.mount("/api", api)
-
-app.mount("/", StaticFiles(directory="static", html = True), name="static")
 
 @api.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
